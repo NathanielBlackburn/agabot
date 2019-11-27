628d13178bbf201c54d41840e068fdfd48010ff0
Izek Inge <izydor@golezwierzeta.pl>
2019-11-27 08:47:19 +0100

    Bugfix: arrayToolkit.dedupe does not work for objects

--- tools/arrayToolkit.js
+++ tools/arrayToolkit.js
@@ -4,9 +4,10 @@ module.exports = {
     return list.length ? list[Math.floor(Math.random() * list.length)] : null;
   },
 
-  dedupe: (list) =>{
+  dedupe: (list) => {
     return list.filter((item, index) => {
-      return list.indexOf(item) === index;
+      const jsonItem = JSON.stringify(item);
+      return list.findIndex(item => JSON.stringify(item) === jsonItem) === index;
     });
   }
 
