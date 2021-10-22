3ba115092dc4b9ac81e00770be1ba1da3dd83f8e
Konrad Rataj <k.rataj@cinkciarz.pl>
2021-10-22 12:49:17 +0200

    [Fix] Config change

--- routing/security/securityCheck.js
+++ routing/security/securityCheck.js
@@ -2,7 +2,7 @@ module.exports = {
 
     conduct: (request, response, config) => {
         const json = request.body;
-        if (!json || !json.token || json.token !== config.hangoutsApiSecret) {
+        if (!json || !json.token || json.token !== config.hangouts.apiSecret) {
             response.status(403).end();
             return false;
         } else {
