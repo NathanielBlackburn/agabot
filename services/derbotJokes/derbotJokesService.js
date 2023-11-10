dcb3a7d1c7eb57037ee20937cb28b0dee0ca149f
Nathaniel Blackburn <nathaniel.a.blackburn@gmail.com>
2023-11-10 15:23:55 +0100

    [Feature] Fix joke API and add another one

--- services/derbotJokes/derbotJokesService.js
+++ services/derbotJokes/derbotJokesService.js
@@ -1,13 +1,29 @@
 const promisifyHttp = require('@tools/promisifyHttp');
-const http = promisifyHttp(require('http'));
+const https = promisifyHttp(require('follow-redirects').https);
 const cheerio = require('cheerio');
+const arrayToolkit = require('@tools/arrayToolkit');
+
+const derbotServices = [
+  {
+    url: 'https://piszsuchary.pl/losuj',
+    selector: 'div.container > main.content article.image-container > figure > figcaption'
+  },
+  {
+    url: 'https://smieszne-kawaly.pl/losuj-kawal',
+    selector: 'div#content > div.post > div.entry'
+  },
+];
 
 module.exports = class DerbotJokesService {
 
   async random() {
-    const jokeData = await http.request('http://piszsuchary.pl/losuj', {});
+    const derbotService = arrayToolkit.randomElement(derbotServices);
+    const jokeData = await https.request(derbotService.url, {});
     const $ = cheerio.load(jokeData);
-    return $('div.kot_na_suchara a img').attr('alt').replace(/^\s*$\r?\n/gm, '');
+    return $(derbotService.selector).first().text()
+      .replace(/^[\s\n]+/g, '')
+      .replace(/\n+/g, '\n')
+      .trim();
   }
 
 };
